f9a6e2133991d77bc17461c56ddf0538eebf5a30
dhgwag <Donghoon.Gwag@gmail.com>
2022-08-24 15:50:12 +0900

    folder name from out to docs

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.listen(30080, function(){
     console.log("Start Server, port : 30080")
 });
 
-app.use(express.static(__dirname + '/out'));
+app.use(express.static(__dirname + '/docs'));
 
 app.get('/', function(req, res){
-    res.sendfile(__dirname + "/out/index.html");
+    res.sendfile(__dirname + "/docs/index.html");
 });
